02a71f50d9e124ebb06f2517a23ee4f3826c9207
Glitch (flask-python3) <none>
2020-10-20 20:17:38 +0000

    💧🐀 Checkpoint ./server.py:23217119/499

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import tweepy
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+@app.route('/')
+def index():
+	
+	
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth)
+	
+	search = request.args.get('q')
+	
+	public_tweets = api.user_timeline(search)
+
+	return render_template('home.html', tweets=public_tweets)
 
 if __name__ == "__main__":
   app.run()
