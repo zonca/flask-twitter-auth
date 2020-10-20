8e00622c65031aaaaccdc4856fe4ae31f4744650
Glitch (flask-python3) <none>
2020-10-20 20:29:36 +0000

    🦀📫 Checkpoint ./requirements.txt:23217119/7 ./server.py:23217119/1041

--- server.py
+++ server.py
@@ -1,21 +1,19 @@
-from flask import Flask, render_template, request
 import tweepy
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	
-	
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
-	api = tweepy.API(auth)
-	
-	search = request.args.get('q')
-	
-	public_tweets = api.user_timeline(search)
+consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
+consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
+callback = 'https://twitter-blocklist-auth.glitch.me/callback'
 
-	return render_template('home.html', tweets=public_tweets)
+@app.route('/auth')
+def auth():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback)
+    url = auth.get_authorization_url()
+    session['request_token'] = auth.request_token
+    return redirect(url)
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
